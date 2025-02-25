1b60b8cf5d223bb51547d547cf4511daf4cf79d1
WenrongZou <wenrongzou@outlook.com>
2025-02-25 23:53:37 +0100

    Update project

--- FormalGroups/FG.lean
+++ FormalGroups/FG.lean
@@ -1,2 +1,23 @@
-import Mathlib
-#check Nat.add_comm
+/-
+Copyright (c) 2025 Wenrong Zou. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Wenrong Zou
+-/
+
+import Mathlib.Algebra.MvPolynomial.Eval
+
+/-!
+
+## One Dimensional Formal Group
+This file defines one dimensional formal group law over a ring `A`, homomorphism and isomorphism between two formal group.
+
+## Adivisor : María Inés de Frutos-Fernández
+
+## Reference:
+· Silverman, The Arithmetic of Elliptic Curves (2nd edition) - Chapter 4.
+· Lubin--Tate, Formal Complex Multiplication in Local Fields.
+· Hazewinkel, Formal Groups and Applications. Start with Chapters 1 and 2. Later you can look at some parts of Chapters 4 and 6.
+
+-/
+
+-- Definition of Formal Group
